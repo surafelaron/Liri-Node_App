308e08b43a90d903adaf74ccb1c0c7087e8049fe
Surafel Ferede <surafelaron@gmail.com>
2019-06-04 21:54:15 -0700

    final commit

--- liri.js
+++ liri.js
@@ -1,10 +1,9 @@
-
 //
 require("dotenv").config();
 //
 var keys = require("./keys");
 //
-var Spotify= require("node-spotify-api");
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var fs = require("fs");
@@ -12,31 +11,34 @@ var moment = require("moment");
 // variable for the command
 var instruction = process.argv[2];
 //variable for search
- var userSearch = process.argv.splice(3).join(" ");
+var userSearch = process.argv.splice([3]).join(" ");
 //
-function liriConditions(instruction, userSearch){
-    switch(instruction){
+function liriConditions(instruction, userSearch) {
+    switch (instruction) {
         case "spotify-this-song":
             spotifySong(userSearch);
             break
-            case "movie-this":
-                omdbMovies(userSearch);
-                break
-                case "concert-this":
-                    concert(userSearch);
-                    break
-                    case "do-what-it-says":
-                        doWhatSay(userSearch);
+        case "movie-this":
+            omdbMovies(userSearch);
+            break
+        case "concert-this":
+            concert(userSearch);
+            break
+        case "do-what-it-says":
+            doWhatSay(userSearch);
     }
 }
 // 
-function spotifySong (songName){
-    if(!songName){
+function spotifySong(songName) {
+    if (!songName) {
         songName = "all of you"
     };
-    spotify.search({type: "track" , query: songName},function(err, data){
-        if(err){
-            return console.log("Error: " + err);
+    spotify.search({
+        type: "track",
+        query: songName
+    }, function (err, data) {
+        if (err) {
+            return console.log(err); 
         }
         console.log("======================================");
         // artstis
@@ -48,37 +50,38 @@ function spotifySong (songName){
         // the album for the song
         console.log("the album name for the song: " + data.tracks.items[0].album.name + "\r\n");
         // append to the text file
-        var randomSong = "\n=====spotify search====== " + "\nartist: " + data.tracks.items[0].album.artists[0].name  + "\nsong name: " + data.tracks.items[0].name  + "\npreview link: " + data.tracks.items[0].href + "\nAlbum: " + data.tracks.items[0].album.name + "\r\n";
-        fs.appendFile("log.txt", randomSong, function(err){
-            if(err){
+        var randomSong = "\n=====spotify search====== " + "\nartist: " + data.tracks.items[0].album.artists[0].name + "\nsong name: " + data.tracks.items[0].name + "\npreview link: " + data.tracks.items[0].href + "\nAlbum: " + data.tracks.items[0].album.name + "\r\n";
+        fs.appendFile("log.txt", randomSong, function (err) {
+            if (err) {
                 console.log(err);
             }
         })
     })
 }
-function concert(musician){
-    var musician = userSearch;
-    var queryURL = "https://rest.bandsintown.com/artists/" + musician + "/events?app_id=codingbootcamp";
-    axios.get(queryURL).then(function (response){
+
+function concert(userSearch) {
+    //var musician = userSearch;
+    var queryURL = "https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp";
+    axios.get(queryURL).then(function (response) {
         console.log("====================================");
-        console.log("Artist: " + musician + "\r\n");
-        console.log("Place for the concert: " + response.data[0].venue.name + "\r\n");
+        console.log("Artist: " + userSearch + "\r\n");
+        console.log("Place for the concert: " + response.data[0].venue.name + "\r\n"); 
         console.log("location: " + response.data[0].venue.city + "\r\n");
         console.log("Date: " + moment(response.data[0].datetime).format("MM-DD-YYYY") + "\r\n");
         // append to log.txt
-        var randomConcert = "\r\n=======concert search======" + "\nName of singer(s): " + musician + "\nPlace for the concert: " +  response.data[0].venue.name + "\nLocation: " + response.data[0].venue.city + "\nDate: " + moment(response.data[0].datetime).format("MM-DD-YYYY");
-        fs.appendFile("log.txt", randomConcert, function(err){
-            if(err){
+        var randomConcert = "\n=======concert search======" + "\nName of singer(s): " + userSearch + "\nPlace for the concert: " + response.data[0].venue.name + "\nLocation: " + response.data[0].venue.city + "\nDate: " + moment(response.data[0].datetime).format("MM-DD-YYYY");
+        fs.appendFile("log.txt", randomConcert, function (err) {
+            if (err) {
                 console.log(err);
             }
         })
-    })
+    }) 
 };
 
-function omdbMovies(movie){
+function omdbMovies(movie) {
     //var movie = userSearch;
-    var movieQueryUrl = "http://www.omdbapi.com/?t="+movie+"&y=&plot=short&apikey=trilogy";
-    axios.request(movieQueryUrl).then(function(response){
+    var movieQueryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    axios.request(movieQueryUrl).then(function (response) {
         console.log("================================");
         console.log("Title: " + response.data.Title + "\r\n");
         console.log("Release Year: " + response.data.Year + "\r\n");
@@ -89,24 +92,27 @@ function omdbMovies(movie){
         console.log("Plot: " + response.data.Plot + "\r\n");
         console.log("Actors: " + response.data.Actors + "\r\n");
         var randomMovies = "\n====movie search========" + "\nTitle: " + response.data.Title + "\nRelease Year: " + response.data.Year + "\nIMDB Rating: " + response.data.imdbRating + "\nRotten Tomato: " + response.data.Ratings[1].value + "\nCountry where produced: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors;
-        fs.appendFile("log.txt",randomMovies, function(err){
-            if(err){
+        fs.appendFile("log.txt", randomMovies, function (err) {
+            if (err) {
                 console.log(err);
             }
-        }) 
+        })
     })
 }
-function doWhatSay(){
-    fs.readFile("rondom.txt", "utf8", function(err,data){
-        if(err){
+
+function doWhatSay() {
+    fs.readFile("rondom.txt", "utf8", function (err, data) {
+        if (err) {
             console.log(err);
-        }else{
-            console.log(data)
-        var randomText = data.split(",");
+        }
+            console.log(data);
+            var randomText = data.split(",")
+//console.log(randomText);
+            liriConditions(randomText[0],randomText[1]);
+            // console.log(liriConditions(randomText[0], randomText[1]))
         
-    liriConditions(randomText[0], randomText[1]);
-    }
-    })
-}
+    });
+    
+};
 
 liriConditions(instruction, userSearch);
